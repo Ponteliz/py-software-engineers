de0aba7744017c7f70319c1bfa5fc4665f155866
unknown <lh3241pon@gmail.com>
2026-07-25 21:03:29 -0300

    Solution 3

--- app/developers/full_stack_developer.py
+++ app/developers/full_stack_developer.py
@@ -5,19 +5,19 @@ class FullStackDeveloper(BackendDeveloper):
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills.extend(
-            [
-                "JavaScript",
-                "CSS",
-                "HTML",
-            ]
-        )
+        self.skills.extend([
+            "JavaScript",
+            "CSS",
+            "HTML",
+        ])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
     def create_web_application(self) -> None:
-        print(f"{self.name} started creating a web application...")
+        print(
+            f"{self.name} started creating a web application..."
+        )
         self.create_powerful_api()
         self.create_awesome_web_page()
